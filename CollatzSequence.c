0b7fee9176c8f18658a464e376f9ec2f567b0f3d
forbes1799 <123934016+forbes1799@users.noreply.github.com>
2024-02-13 08:28:37 +0000

    Add files via upload

--- CollatzSequence.c
+++ CollatzSequence.c
@@ -5,13 +5,17 @@
 #include <stdio.h>
 
 
-/* the function return number based on even/odd test*/
-int test(unsigned int x)
+/*Collatz should return f(x) where 
+f(x) is x/2 if x is even, 
+and f(x) is 3x+1 if x is odd*/
+int collatz(unsigned int x)
 {
     /* write your code here*/
 }
 
-/* the function return the length of the sequnce*/
+/*The function returns the length of the sequence
+/*It should take x, and repeatedly call collatz 
+until x == 1, while counting how many calls it makes*/
 int count_length(unsigned int x)
 {
     /* write your code here*/
@@ -23,13 +27,15 @@ int main(int argc, char* argv[])
 
     for (i = 1; i < 1000000; i++)
     {
+        //Find the length of the collatz sequence starting from i
         unsigned int k = count_length(i);
         
         if (k > count)
         {
+            /*Only print if we find a longer sequence length*/
             count = k;
-            init = i;
-            printf("The sequence length for the number %u equals %u \n", init, count);
+            i;
+            printf("The sequence length for the number %u equals %u \n", i, count);
         }
     }
 

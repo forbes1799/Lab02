13812689e76f4fc4b052e209765b6f62071e2f05
forbes1799 <123934016+forbes1799@users.noreply.github.com>
2026-02-05 10:28:21 +0000

    Update quad-serialOnly.sh

--- quad-serialOnly.sh
+++ quad-serialOnly.sh
@@ -10,11 +10,7 @@
 
 # Specific course queue, exclusive use (for timings), max 1 min wallclock time
 #SBATCH -p course
-#SBATCH --exclusive
 #SBATCH -t 1
-
-# load modules
-module load compilers/intel/2019u5 
 
 # just 1 thread to run on
 export OMP_NUM_THREADS=1
@@ -27,5 +23,3 @@ echo '-------'
 
 # Run...
 # GNU with Optimisation
-# Intel without Optimisation
-# Intel with Optimisation
